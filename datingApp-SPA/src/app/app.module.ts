03479731efb19658a31c0f7d9409710660b8580c
veronica <veronicaoviedolopez@gmail.com>
2020-02-14 16:49:46 -0600

    Update app.module.ts

--- datingApp-SPA/src/app/app.module.ts
+++ datingApp-SPA/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './list/list.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
